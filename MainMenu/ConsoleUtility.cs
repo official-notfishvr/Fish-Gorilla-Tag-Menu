bfd6461f7474a63a8c80e2de2b0ff0ea539c77b8
notfishvr <124737490+official-notfishvr@users.noreply.github.com>
2024-02-16 23:08:55 -0600

    Update ConsoleUtility.cs

--- MainMenu/ConsoleUtility.cs
+++ MainMenu/ConsoleUtility.cs
@@ -13,68 +13,6 @@ namespace Fish_Menu.MainMenu
 {
     public class ConsoleUtility : MonoBehaviour
     {
-        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern int AllocConsole();
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool SetConsoleTitle(string lpConsoleTitle);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool WriteConsole(IntPtr hConsoleOutput, string lpBuffer, uint nNumberOfCharsToWrite, out uint lpNumberOfCharsWritten, IntPtr lpReserved);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr GetStdHandle(int nStdHandle);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, uint wAttributes);
-
-        public static void OpenConsoleWindow()
-        {
-            ConsoleUtility.AllocConsole();
-            ConsoleUtility.SetConsoleTitle("Console [Initializing]");
-            ConsoleUtility.WriteToConsole("Debug log started!", ConsoleColor.Green);
-            Debug.Log("Console was started!");
-            ConsoleUtility.SetConsoleTitle("Console [Ready]");
-        }
-        public static void WriteToConsole(string message, ConsoleColor color)
-        {
-            IntPtr stdHandle = ConsoleUtility.GetStdHandle(-11);
-            message += "\n";
-            ConsoleUtility.SetConsoleTextAttribute(stdHandle, (uint)color);
-            uint num;
-            ConsoleUtility.WriteConsole(stdHandle, message, (uint)message.Length, out num, IntPtr.Zero);
-            ConsoleUtility.SetConsoleTextAttribute(stdHandle, 15U);
-        }
-        public static void WriteLine(string text)
-        {
-            string text2 = "[INFO]: " + text;
-            IntPtr stdHandle = ConsoleUtility.GetStdHandle(-11);
-            text2 += "\n";
-            ConsoleUtility.SetConsoleTextAttribute(stdHandle, 15U);
-            uint num;
-            ConsoleUtility.WriteConsole(stdHandle, text2, (uint)text2.Length, out num, IntPtr.Zero);
-            ConsoleUtility.SetConsoleTextAttribute(stdHandle, 15U);
-        }
-        public static void LCE(string message)
-        {
-            IntPtr stdHandle = ConsoleUtility.GetStdHandle(-11);
-            message += "\n";
-            ConsoleUtility.SetConsoleTextAttribute(stdHandle, 12U);
-            uint num;
-            ConsoleUtility.WriteConsole(stdHandle, message, (uint)message.Length, out num, IntPtr.Zero);
-            ConsoleUtility.SetConsoleTextAttribute(stdHandle, 15U);
-        }
-        public static IntPtr FindConsoleWindow(string windowName)
-        {
-            return ConsoleUtility.FindWindow(null, windowName);
-        }
-        public static IntPtr GetConsole()
-        {
-            return ConsoleUtility.FindWindow("cmd", null);
-        }
-        private static int co;
+    // had to remove bc of fucking okaa his a fucking ass
     }
 }
